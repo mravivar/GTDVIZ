dead648991dc51b7b19b30ab6e34c7a08c5e3ac2
aravindGear4 <athanga1@asu.edu>
2017-03-11 21:35:48 -0700

    added two versions of d3

--- js/coffee.js
+++ js/coffee.js
@@ -1,54 +1,38 @@
+'use strict';
+//DEFINE YOUR VARIABLES UP HERE
 var chart, vis;
 var height = 500;
 var width = 900;
 var margin = {top: 20, right: 50, bottom: 30, left: 30};
-//DEFINE YOUR VARIABLES UP HERE
 
 //Gets called when the page is loaded.
 function init(){
-  chart = d3.select('#vis').append('svg')
+  chart = d3_v4.select('#vis').append('svg')
           .attr("width", width + margin.left + margin.right)
           .attr("height", height + margin.top + margin.bottom)
           .append("g")
           .attr("transform", "translate(30,30)");;
   vis = chart.append("g");
-
-// read in world-countries.topojson
-/*
-var City="City (en)"
-function type(d){
-    d.Latitude = +d.Latitude;
-    d.Longitude = +d.Longitude;
-    d.City = d.City;
+  update_worldMap();
+  console.log(store);
 }
-*/
-/*
-  d3.queue()
-    .defer(d3.json, "world-countries.topojson")
-    .defer(d3.csv, "capitals1.csv")
-    .await(ready)
-*/
 
-
-
-d3.queue()
-  .defer(d3.json, "../data/world-countries.topojson")
-  .await(ready)
-
-
-
+function update_worldMap(){
+d3_v4.queue()
+  .defer(d3_v4.json, "../data/world-countries.topojson")
+  .await(ready);
 //function ready(error,data,capitals) {
 function ready(error,data) {
-  console.log(data);
+  //console.log(data);
 
   var countries = topojson.feature(data, data.objects.countries1).features
-  console.log(countries)
+  //console.log(countries)
 
-  var projection = d3.geoMercator()
+  var projection = d3_v4.geoMercator()
                      .translate([width/2,height/2])
                      .scale(150)
 
-  var path = d3.geoPath()
+  var path = d3_v4.geoPath()
   .projection(projection)
 
   chart.selectAll(".country")
@@ -57,11 +41,11 @@ function ready(error,data) {
      .attr("class", "country")
      .attr("d",path)
      .on('mouseover', function(d){
-       d3.select(this).classed("selected", true)
+       d3_v4.select(this).classed("selected", true)
 
      })
      .on('mouseout', function(d){
-       d3.select(this).classed("selected", false)
+       d3_v4.select(this).classed("selected", false)
      })
 
 
@@ -74,23 +58,23 @@ function ready(error,data) {
              endyr:$('#endyr').val()
            },
            success: function(capitals) {
-               console.log(capitals)
+               //console.log(capitals)
                chart.selectAll(".city-circle").data(capitals)
                .enter().append("circle")
                .attr("r",2)
                .attr("cx",function(d){
                    var coords = projection([d.longitude, d.latitude])
-                   console.log(coords)
+                   //console.log(coords)
                    return coords[0];
                })
                .attr("cy",function(d){
                    var coords = projection([d.longitude, d.latitude])
-                   console.log(coords)
+                   //console.log(coords)
                    return coords[1];
                })
            },
            error: function(jqXHR, textStatus, errorThrown) {
-               alert('error ' + textStatus + " " + errorThrown);
+               console.log('error ' + textStatus + " " + errorThrown);
            }
        });
 
