525b47f0c8e3270113e0c0903ad1f756953be7ea
Deepak Soundararajan <sdeepak94@hotmail.com>
2017-03-22 18:26:38 -0700

    Added Themeriver functionality

--- js/coffee.js
+++ js/coffee.js
@@ -14,7 +14,97 @@ function init(){
           .attr("transform", "translate(30,30)");;
   vis = chart.append("g");
   update_worldMap();
+// themeriver("../data/data.csv");
   console.log(store);
+}
+var datearray = [];
+var colorrange = [];
+
+
+function themeriver(data) {
+
+colorrange = ["#a6cee3","#1f78b4","#b2df8a","#33a02c","#fb9a99","#e31a1c","#fdbf6f","#ff7f00","#ff7f00","#6600cc","#66ff99"];
+//["#B30000", "#78C679", "#FC8D59", "#FDBB84", "#CC4C02", "#FEF0D9","#980043", "#DD1C77", "#DF65B0", "#02F4C7","#238443","#EFFFFF"];
+var strokerange = ["FFFFFF"]
+var strokecolor = strokerange[0];
+
+var format = d3.time.format("%m/%d/%Y");
+
+var margin = {top: 20, right: 40, bottom: 30, left: 30};
+var width = document.body.clientWidth - margin.left - margin.right;
+var height = 400 - margin.top - margin.bottom;
+
+var x = d3.time.scale()
+    .range([0, width]);
+
+var y = d3.scale.linear()
+    .range([height, 0]);
+
+var z = d3.scale.ordinal()
+    .range(colorrange);
+
+var xAxis = d3.svg.axis()
+    .scale(x)
+    .orient("bottom")
+    .ticks(d3.time.years);
+
+var yAxis = d3.svg.axis()
+    .scale(y);
+
+var stack = d3.layout.stack()
+    .offset("zero")
+    .values(function(d) { return d.values; })
+    .x(function(d) { return d.date; })
+    .y(function(d) { return d.numEvents; });
+//console.log(stack);
+var nest = d3.nest().key(function(d) {  return d.key; })
+
+var area = d3.svg.area()
+    .interpolate("cardinal")
+    .x(function(d) { return x(d.date); })
+    .y0(function(d) { return y(d.y0); })
+    .y1(function(d) { return y(d.y0 + d.y); });
+
+var svg = d3.select(".themeriver").append("svg")
+    .attr("width", width + margin.left + margin.right)
+      .attr("height", height + margin.top + margin.bottom)
+  .append("g")
+    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+  data.forEach(function(d) {
+    var mdate = "01/01/"+d.date
+    d.date = format.parse(mdate);
+    if(isNaN(d.numEvents)){
+      d.numEvents=+0;
+    }
+    else{
+    d.numEvents = +d.numEvents;
+  }
+  });
+  console.log(data); 
+  var layers = stack(nest.entries(data));
+  console.log(layers);
+  x.domain(d3.extent(data, function(d) { return d.date; }));
+  y.domain([0, d3.max(data, function(d) { return d.y0 + d.y; })]);
+
+  svg.selectAll(".layer")
+      .data(layers)
+    .enter().append("path")
+      .attr("class", "layer")
+      .attr("d", function(d) { return area(d.values); })
+      .style("fill", function(d, i) { return z(i); });
+
+  svg.append("g")
+      .attr("class", "x axis")
+      .attr("transform", "translate(0," + height + ")")
+      .call(xAxis);
+
+  svg.append("g")
+      .attr("class", "y axis")
+      .attr("transform", "translate(" + width + ", 0)")
+      .call(yAxis.orient("right"));
+
+
 }
 
 function update_worldMap(){
@@ -94,5 +184,6 @@ function ready(error,data) {
          return coords[1];
      })
      */
+
 }
 }
