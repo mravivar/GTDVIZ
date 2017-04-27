b22d62cf00da5c104c6e4e7b7ea45dbcf56fd2a3
aravindGear4 <athanga1@asu.edu>
2017-04-26 19:59:39 -0700

    added references

--- js/detailsView.js
+++ js/detailsView.js
@@ -1,8 +1,9 @@
 'use strict';
 var grid, dataView;
 //TODO if the user clicks on a certain row then display that full column in details
+//some codes in this file are referred from https://github.com/mleibman/SlickGrid/wiki/DataView
 function loadDataIntoDetailsView(data){
-
+    // following 10 lines of code https://github.com/mleibman/SlickGrid/wiki/DataView
   if(dataView){
     dataView.beginUpdate();
     dataView.setItems(data, ['eventid']);
@@ -30,7 +31,7 @@ function loadDataIntoDetailsView(data){
       cellFlashingCssClass: "flashMe",
     };
     grid = new Slick.Grid("#detailsTable", dataView, columns, options);
-
+//following 8 lines of code is got from https://github.com/mleibman/SlickGrid/wiki/DataView
     // Make the grid respond to DataView change events.
     dataView.onRowCountChanged.subscribe(function (e, args) {
       grid.updateRowCount();
@@ -64,7 +65,7 @@ function loadDataIntoDetailsView(data){
       });
       // or dataView.getItem(args.row);
     });
-    //the below function is got from Stackoverflow.com
+    //the below 5 lines of code is got from https://github.com/mleibman/SlickGrid/wiki/DataView
     grid.onSort.subscribe(function(e, args) {
           var comparer = function(a, b) {
               return (a[args.sortCol.field] > b[args.sortCol.field]) ? 1 : -1;
